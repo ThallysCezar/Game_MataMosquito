b9b7a90b9659a028c2c03dd4c5561fb0df21921a
ThallysCezar <75546628+ThallysCezar@users.noreply.github.com>
2022-03-25 08:14:42 -0300

    +code2

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-// para sabermos qual a altura(X) e largura(Y) do browser, iremos usar o window.
+// para sabermos qual a altura(X) e largura(Y) do browser, iremos usar o window. Porém, como não atualiza e fica fixo o valor, mesmo que o usuario redimensione a tela, entao teremos que prever isso e fazer com que recarregue toda vez que o usuário mexer. Logo teremos que fazer dentro de uma função para assim acrescentar mais umas coisas.
 
-var alt = window.innerHeight;
-var larg = window.innerWidth;
+var alt = 0;
+var larg = 0;
 
-console.log(alt, larg);
+function ajustarTamanho(){
+    alt = window.innerHeight;
+    larg = window.innerWidth;
+
+    console.log(alt, larg);
+}
+
+ajustarTamanho();
+
