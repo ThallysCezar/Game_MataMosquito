15cecda80a30c47adf75ba2928cf52e0167a4b4d
ThallysCezar <75546628+ThallysCezar@users.noreply.github.com>
2022-03-25 10:51:12 -0300

    +code18

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 var alt = 0;
 var larg = 0;
 var vidas = 1;
+var tempo = 10;
 
 function ajustarTamanho() {
     alt = window.innerHeight;
@@ -13,6 +14,17 @@ function ajustarTamanho() {
 }
 
 ajustarTamanho();
+
+var cronometro = setInterval(function(){
+    tempo -=1
+    if(tempo < 0){
+        clearInterval(cronometro);
+        clearInterval(criarMosquito);
+    } else{
+        document.getElementById('cronometro').innerHTML = tempo;
+    }
+},
+1000);
 
 function posicaoRandom() {
 
